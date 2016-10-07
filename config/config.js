536b46af23636e1ff5b3b1d4c9830622cd40fa74
Tanner Plauche <tplauche@vschool.io>
2016-10-07 10:07:43 -0600

    update

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 angular.module("DaftPunkSite")
 
-.config(function($routeProvider){
+.config(function($routeProvider) {
     $routeProvider
-    .when("/home", {
-        templateUrl: "./templates/home.html"
-    })
-    .when("/about", {
-        templateUrl: "./templates/about.html"
-    })
-    .when("/contact", {
-        templateUrl: "./templates/contact.html"
-    })
-    .when("/tour", {
-        templateUrl: "./templates/tour.html"
-    })
-    .otherwise("/home", {
-        templateUrl: "./templates/home.html"
-    });
+        .when("/home", {
+            templateUrl: "./templates/home.html"
+        })
+        .when("/about", {
+            templateUrl: "./templates/about.html"
+        })
+        .when("/contact", {
+            templateUrl: "./templates/contact.html"
+        })
+        .when("/tour", {
+            templateUrl: "./templates/tour.html"
+        })
+        .otherwise("/home", {
+            templateUrl: "./templates/home.html"
+        });
 })
